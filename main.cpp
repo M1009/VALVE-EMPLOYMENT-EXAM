3e0b6a29e3c8d11f6dac97c18a78a5ac887ad4c8
Eric <earnaud@wolfnet.io>
2022-11-30 15:42:18 -0800

    Removed old broken main

--- main.cpp
+++ main.cpp
@@ -1,36 +1,39 @@
-// Valve Employment Exam.cpp: PLEASE THE GABEN SO HE MAY DISTRUBTE HIS LOVE THROUGH STEAM SALES.......
+// VALVE EMPLOYMENT WINDOWS.cpp : PLEASE THE GABEN SO HE MAY DISTRUBTE HIS LOVE THROUGH STEAM SALES.......
+#include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
-
+#include <string>
 using namespace std;
 
-int main(){ // main function for program.
-	int i;
-	printf("Valve employment exam: Please enter a number in integer format...\nAny number that exists at all....\n");
+int i;
+void wrong() { cout << "WRONG!\n" << endl;
+	cout << "Trick!! 2 is in fact the largest number in the universe.\n\nThanks for your interest in Valve, we regret to inform you....\nThe Gaben is displeased. You now work at EA's Origin, so sad.\n";
+	cout << "GAME OVER!\n";
+}
+void correct() { std::string line;
+	std::ifstream GABE;
+	cout << "CORRECT!\n";
+	cout << "Congratulations, you know the truth.\nThanks for your interest in Valve.\n\nCongratulations! We have selected you for a interview with the Gaben......\n";
+	GABE.open("Gaben.txt");
+	getline(GABE, line);
+	// This loop handles the prize for victory condition.
+	if (GABE.is_open()) {
+		while (getline(GABE, line)) {
+			cout << line << endl;
+		} GABE.close();
+	} else { cout << "ERROR: Unable to open file\n";
+	}
+	cin.get();
+}
+
+
+int main() { cout << "Valve employment exam: Please enter a number in integer format...\nAny number that exists at all....\n";
 	cin >> i;
 	cout << i << "?!?! Thats your answer?!\n\n";
-
-	if (i >= 3) {cout << "WRONG!\n" << endl; /* This IF/ELSEIF/ELSE loop handles the victory/defeat conditions. */
-		printf("Trick!! 2 is in fact the largest number in the universe.\n\nThanks for your interest in Valve, we regret to inform you....\nThe Gaben is displeased. You now work at EA's Origin, so sad.\n");
-		printf("GAME OVER!");
-		getchar();
-	}   else {printf("CORRECT!\n");
-		printf("Congratulations, you know the truth.\nThanks for your interest in Valve.\n\nCongratulations! We have selected you for a interview with the Gaben......\n");
-		cin:getline();
-		string line;
-		ifstream GABE;
-		GABE.open("Gaben.txt");
-    	if (GABE.is_open()){ // This loop handles the prize for victory condition.
-        	while (getline (GABE,line)){
-		cout << line << '\n';
-    	}  	GABE.close();
-    	} else cout << "ERROR: Unable to open file\n";
+	// This IF/ELSEIF/ELSE loop handles the victory/defeat condition.
+	if (i >= 3) { wrong();
+	} else { correct();
 	}
-    cout << "GAME OVER!" << endl;
-	return 0;
-	}
-
-
-
-
+	cin.get();
+}
